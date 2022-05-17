71b847dd7b0399da9e00ce9ad058d4764c2d2dbe
mabag24 <matthewbaggott@me.com>
2022-05-17 12:26:55 +0100

    added prettier

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require("gulp");
 const nunjucksRender = require("gulp-nunjucks-render");
 const browserSync = require("browser-sync").create();
 const imagemin = require("gulp-imagemin");
+const prettier = require("gulp-prettier");
 
 const sass = require("gulp-sass")(require("sass"));
 
@@ -18,10 +19,16 @@ function build() {
 				path: ["src/html/components"],
 			})
 		)
+		.pipe(prettier())
 		.pipe(gulp.dest("dist"));
 }
 function style() {
-	return gulp.src("./src/sass/styles.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./dist/css")).pipe(browserSync.stream());
+	return gulp
+		.src("./src/sass/styles.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(prettier({ singleQuote: true }))
+		.pipe(gulp.dest("./dist/css"))
+		.pipe(browserSync.stream());
 }
 
 function browsersyncServer(cb) {
