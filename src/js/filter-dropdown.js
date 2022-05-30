93f0bba40ba134a438bc72a0c3bfe95a0517402c
mabag24 <matthewbaggott@me.com>
2022-05-30 10:20:11 +0100

    media-queries added

--- src/js/filter-dropdown.js
+++ src/js/filter-dropdown.js
@@ -1,8 +1,13 @@
 let deg = 360;
+let filterArr = ["Close filters", "Open filters"];
 document.getElementById("filter-dropdown").addEventListener("click", () => {
 	deg -= 180;
 	document.getElementsByClassName("filters")[0].classList.toggle("invisible");
 	document.getElementsByClassName("filter-icon")[0].style.transform = `rotate(${deg}deg)`;
+	document.getElementById("filter-text").innerHTML = filterArr[0];
+	let temp = filterArr[0];
+	filterArr[0] = filterArr[1];
+	filterArr[1] = temp;
 	if (deg == 0) {
 		deg = 360;
 	}
